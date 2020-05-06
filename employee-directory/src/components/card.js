a1c4bbf0f38ca495f4ed607362d6b27f6244f991
marcustate <58437889+marcustate@users.noreply.github.com>
2020-05-05 21:59:58 -0500

    adding components folder with card.js

---
+++ employee-directory/src/components/card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Card(props) {
+    return
+    (
+            <tr>
+                <th scope="row"><img alt={props.firstName} src={props.picture} /> </th>
+                <td > {props.firstName}</td>
+                <td > {props.lastName}</td>
+                <td > {props.emailName}</td>
+            </tr>       
+    )
+}

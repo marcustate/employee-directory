194c112093a7aeda87e78c5b57808d498dc77fe5
marcustate <58437889+marcustate@users.noreply.github.com>
2020-05-19 18:16:56 -0500

    updated coontainer.js

--- employee-directory/src/components/Container.js
+++ employee-directory/src/components/Container.js
@@ -14,7 +14,7 @@ class Container extends Component{
         filterBy: "lastName",
         currentSort: "default",
         sort: ""
-    },
+    };
 
     componentWillMount() 
     {
@@ -34,8 +34,96 @@ class Container extends Component{
             })
             .catch(er => console.log(err));
     }
-    filterEmployees = (search) => {
+    filterEmployees = (searchkey) => {
+        console.log("***in Filter*******");
         console.log(searchkey);
-        var 
+        console.log(this.state.result);
+        var filterResult = this.state.result.filter(person => person.firstName === searchkey)
+    
+        this.setState({
+          result:filterResult
+          
+        })}
+    
+      handleFormSubmit = event => {
+        event.preventDefault();
+        const value = event.target.value;
+        const name = event.target.name;
+        console.log("**********");
+        console.log(value);
+        console.log(name);
+        this.filterEmployees(value);
+        this.setState({
+    
+          [name]: value
+    
+        });
+        this.filterEmployees(value);
+        this.filterEmployees(this.state.search);
+    
+      };
+      handleInputChange = event => {
+        event.preventDefault();
+        console.log(event);
+        const value = event.target.value;
+        const name = event.target.name;
+        console.log(value);
+        console.log(name);
+        this.setState({
+    
+          [name]: value
+    
+        });
+            
+      };
+    
+      render() {
+    
+        return (
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h2>Employee Directory</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <Form
+                  value={this.state.search}
+                   handleInputChange={this.handleInputChange}
+                   handleFormSubmit={this.handleFormSubmit}
+                />
+              </div>
+            </div>
+    
+            <div className="row">
+              <table className="table">
+                <tr>
+                  <th scope="col">Photo</th>
+                  <th>First Name</th>
+                  <th scope="col">Last Name </th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Phone</th>
+                </tr>
+
+                {[...this.state.result].map((item) =>
+                  <Card
+                    picture={item.picture}
+                    firstName={item.firstName}
+                    lastName={item.lastName}
+                    email={item.email}
+                    phone={item.phone}
+                    key={item.key}
+                  />
+                )}
+    
+              </table>
+            </div>
+    
+    
+          </div>
+        );
+      }
     }
-}
+    
+    export default Container;
